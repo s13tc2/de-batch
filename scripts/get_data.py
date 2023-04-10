1391aab205da1c252a5425830ac5528db888de63
s13tc2 <s13tc2@protonmail.com>
2023-04-09 22:12:20 -0400

    add changes

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -3,11 +3,13 @@ import pathlib
 import numpy as np
 import pandas as pd
 from fredapi import Fred
-from config import CFG
+# from config import CFG
 
 pathlib.Path("./data").mkdir(parents=True, exist_ok=True)
 
-fred = Fred(CFG.api_key)
+api_key = os.environ['FRED_API_KEY']
+
+fred = Fred(api_key)
 sp500 = fred.get_series('SP500')
 sp500.to_csv('./data/sp500.csv')
 print("Success!")
