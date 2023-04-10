99156adfe28cf771a0576ea41960bb5facbd786e
s13tc2 <s13tc2@protonmail.com>
2023-04-10 16:19:03 -0400

    add changes

--- dags/utils/utils.py
+++ dags/utils/utils.py
@@ -6,7 +6,8 @@ from airflow.hooks.S3_hook import S3Hook
 def _local_to_s3(
     bucket_name: str, key: str, file_name: str, remove_local: bool = False
 ) -> None:
-    s3 = S3Hook()
+    my_conn_id = 'my_aws_conn'
+    s3 = S3Hook(my_conn_id)
     s3.load_file(
         filename=file_name, bucket_name=bucket_name, replace=True, key=key
     )
